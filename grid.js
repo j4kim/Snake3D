37d4b083f684b5922495a8f752dce201c99adb93
joaquim <joaquim.perez@he-arc.ch>
2016-11-29 13:32:29 +0100

    classes Grid et Cube dessine la grille et les éléments du serpent

---
+++ grid.js
@@ -0,0 +1,122 @@
+class Drawable{
+	constructor(size){
+		this.vertexBuffer = null;
+		this.indexBuffer = null;
+		this.colorBuffer = null;
+		
+		//Creation of a movement matrix specific for the object
+		this.mvMatrix = mat4.create();
+		
+		/*
+		glContext.POINTS: Draws a single dot.
+		glContext.LINE_STRIP: Draws a straight line to the next vertex.
+		glContext.LINE_LOOP: Draws a straight line to the next vertex, and connects the last vertex back to the first.
+		glContext.LINES: Draws a line between a pair of vertices.
+		glContext.TRIANGLE_STRIP
+		glContext.TRIANGLE_FAN
+		glContext.TRIANGLES: Draws a triangle for a group of three vertices.
+		*/
+	}
+	
+	draw(){
+		
+		glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, this.mvMatrix);
+		glContext.bindBuffer(glContext.ARRAY_BUFFER, this.vertexBuffer);
+		glContext.vertexAttribPointer(prg.vertexPositionAttribute, 3, glContext.FLOAT, false, 0, 0);
+		glContext.bindBuffer(glContext.ARRAY_BUFFER, this.colorBuffer);
+		glContext.vertexAttribPointer(prg.colorAttribute, 4, glContext.FLOAT, false, 0, 0);
+		glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+		glContext.drawElements(this.mode, this.indices.length, glContext.UNSIGNED_SHORT,0);
+	}
+	
+}
+
+class Grid extends Drawable{
+	constructor(size){
+		super();
+		this.size = size;
+		this.init();
+		this.mode=2;
+	}
+	
+	init(){
+		//Initialisation of the arrays used to construct the object
+		this.indices = [];
+		this.vertices = [];
+		this.colors = [];
+		
+		var s=this.size;
+	
+		this.vertices.push(0,0,s);   
+		this.vertices.push(s,0,s);   
+		this.vertices.push(s,0,0);  
+		this.vertices.push(0,0,0);   
+		this.vertices.push(0,s,s);   
+		this.vertices.push(s,s,s); 
+		this.vertices.push(s,s,0); 
+		this.vertices.push(0,s,0);
+
+
+		this.colors.push(1.0, 0.0, 0.0, 1.0);
+		this.colors.push(1.0, 1.0, 0.0, 1.0);
+		this.colors.push(0.0, 1.0, 0.0, 1.0);
+		this.colors.push(0.0, 0.0, 0.0, 1.0);
+		this.colors.push(1.0, 0.0, 1.0, 1.0);
+		this.colors.push(1.0, 1.0, 1.0, 1.0);
+		this.colors.push(0.0, 1.0, 1.0, 1.0);
+		this.colors.push(0.0, 0.0, 1.0, 1.0);
+
+		this.indices.push(4,0,1,4,5,1,6,5,4,6,7,4,0,7,3,0,1,3,2,1,6,2,3,6,7,3);
+
+		this.vertexBuffer = getVertexBufferWithVertices(this.vertices);
+		this.colorBuffer  = getVertexBufferWithVertices(this.colors);
+		this.indexBuffer  = getIndexBufferWithIndices(this.indices);
+	}
+}
+
+class Cube extends Drawable{
+	constructor(x,y,z){
+		super();
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.mode = 3;
+		this.init();
+	}
+	
+	init(){
+		this.indices = [];
+		this.vertices = [];
+		this.colors = [];
+		
+		var x = this.x;
+		var y = this.y;
+		var z = this.z;
+	
+		this.vertices.push(x+0, y+0, z+1);   
+		this.vertices.push(x+1, y+0, z+1);   
+		this.vertices.push(x+1, y+0, z+0);  
+		this.vertices.push(x+0, y+0, z+0);   
+		this.vertices.push(x+0, y+1, z+1);   
+		this.vertices.push(x+1, y+1, z+1); 
+		this.vertices.push(x+1, y+1, z+0); 
+		this.vertices.push(x+0, y+1, z+0);
+
+
+		this.colors.push(1.0, 0.0, 0.0, 0.5);
+		this.colors.push(1.0, 1.0, 0.0, 0.5);
+		this.colors.push(0.0, 1.0, 0.0, 0.5);
+		this.colors.push(0.0, 0.0, 0.0, 0.5);
+		this.colors.push(1.0, 0.0, 1.0, 0.5);
+		this.colors.push(1.0, 1.0, 1.0, 0.5);
+		this.colors.push(0.0, 1.0, 1.0, 0.5);
+		this.colors.push(0.0, 0.0, 1.0, 0.5);
+
+		this.indices.push(4,0,1,4,5,1,6,5,4,6,7,4,0,7,3,0,1,3,2,1,6,2,3,6,7,3);
+
+		this.vertexBuffer = getVertexBufferWithVertices(this.vertices);
+		this.colorBuffer  = getVertexBufferWithVertices(this.colors);
+		this.indexBuffer  = getIndexBufferWithIndices(this.indices);
+	}
+}
---
+++ scene.js
@@ -0,0 +1,82 @@
+
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+var SIZE = 6;
+var sceneObjects = [];
+
+initWebGL();
+
+
+function initShaderParameters(prg){
+	prg.vertexPositionAttribute = glContext.getAttribLocation(prg, "aVertexPosition");
+	glContext.enableVertexAttribArray(prg.vertexPositionAttribute);
+	prg.colorAttribute 			= glContext.getAttribLocation(prg, "aColor");
+	glContext.enableVertexAttribArray(prg.colorAttribute);
+	prg.pMatrixUniform          = glContext.getUniformLocation(prg, 'uPMatrix');
+	prg.mvMatrixUniform         = glContext.getUniformLocation(prg, 'uMVMatrix');
+}
+
+function initScene(){
+
+	mat4.identity(pMatrix);
+	mat4.perspective(pMatrix, degToRad(90), c_width / c_height, 0.1, 100.0);
+	mat4.identity(mvMatrix);
+
+	mat4.translate(pMatrix,pMatrix,vec3.fromValues(-SIZE/2, -SIZE/2, -1.671*SIZE));
+	
+	glContext.enable(glContext.DEPTH_TEST);
+	
+	sceneObjects.push(new Grid(SIZE));
+	sceneObjects.push(new Cube(0,0,0));
+	sceneObjects.push(new Cube(1,0,0));
+	sceneObjects.push(new Cube(2,0,0));
+	sceneObjects.push(new Cube(3,0,0));
+	sceneObjects.push(new Cube(3,1,0));
+	sceneObjects.push(new Cube(4,1,0));
+	sceneObjects.push(new Cube(5,1,0));
+	sceneObjects.push(new Cube(5,2,0));
+	sceneObjects.push(new Cube(5,3,0));
+	sceneObjects.push(new Cube(5,4,0));
+	sceneObjects.push(new Cube(5,4,1));
+	sceneObjects.push(new Cube(5,4,2));
+	sceneObjects.push(new Cube(5,4,3));
+	sceneObjects.push(new Cube(4,4,3));
+	sceneObjects.push(new Cube(3,4,3));
+	sceneObjects.push(new Cube(2,4,3));
+	sceneObjects.push(new Cube(2,4,4));
+	sceneObjects.push(new Cube(2,3,4));
+	sceneObjects.push(new Cube(1,3,4));
+	sceneObjects.push(new Cube(1,2,4));
+	sceneObjects.push(new Cube(0,2,4));
+	sceneObjects.push(new Cube(0,2,5));
+	sceneObjects.push(new Cube(0,1,5));
+	sceneObjects.push(new Cube(0,0,5));
+	sceneObjects.push(new Cube(1,0,5));
+	sceneObjects.push(new Cube(2,0,5));
+	sceneObjects.push(new Cube(3,0,5));
+	sceneObjects.push(new Cube(4,0,5));
+	sceneObjects.push(new Cube(5,0,5));
+	sceneObjects.push(new Cube(5,0,4));
+	sceneObjects.push(new Cube(5,0,3));
+
+	glContext.clearColor(0.2, 0.2, 0.2, 1.0);
+  
+}
+
+function drawScene(){
+	glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
+
+	glContext.uniformMatrix4fv(prg.pMatrixUniform, false, pMatrix);
+	
+	sceneObjects.forEach(function(o){
+		o.draw();
+	});
+}
+
+function initWebGL(){
+	glContext = getGLContext('webgl-canvas');
+	initProgram();
+	initScene();
+	renderLoop();
+}
